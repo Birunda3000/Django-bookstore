63aa65996620986ea9a211049488be2085bbcf90
Erick <38795106+Birunda3000@users.noreply.github.com>
2021-01-14 16:02:58 -0300

    primeiro commit
    Commit de rascunho da pagina de livros

---
+++ loja/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+# Create your models here.
+
+class Genero(models.Model):
+    nome = models.CharField(max_length=50)
+    data_criação = models.DateTimeField(auto_now_add=True)
+    descrição = models.TextField(null=True, blank=True)
+    class Meta:
+        verbose_name_plural = 'Generos'
+    #resolvendo o object no admim nome melhor
+    def __str__(self):
+        return self.nome
+
+class Editora(models.Model):
+    nome = models.CharField(max_length=100)
+    data_criação = models.DateTimeField(auto_now_add=True)
+    endereço = models.CharField(max_length=300)
+    class Meta:
+        verbose_name_plural = 'Editoras'
+    #resolvendo o object no admim nome melhor
+    def __str__(self):
+        return self.nome
+
+class Livro(models.Model):
+    titulo = models.CharField(max_length=50)
+    autor = models.CharField(max_length=50)
+    genero = models.ForeignKey(Genero, on_delete=models.CASCADE)
+    editora = models.ForeignKey(Editora, on_delete=models.CASCADE)
+    sinopse = models.TextField(null=True, blank=True)
+    data_criação = models.DateTimeField(auto_now_add=True)
+
+    preço = models.DecimalField(decimal_places=2, max_digits=9)
+    image = models.FileField(upload_to='products/', null=True, blank=True)
+    image2 = models.FileField(upload_to='products/', null=True, blank=True)
+    image3 = models.FileField(upload_to='products/', null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = 'Livros'
+    #resolvendo o object no admim nome melhor
+    def __str__(self):
+        return self.titulo
---
+++ loja/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+import datetime
+from .models import Genero, Editora, Livro
+# Create your views here.
+
+def home(request):
+    data = {}
+    data['livros'] = Livro.objects.all()
+
+    #html = "<html><body>    meu primiero site dijango It is now %s.</body></html>" % now
+    #return HttpResponse(html)
+    return render(request, 'loja/home.html', data)
+
+def book_page(request, pk):
+    data = {}
+    livro = Livro.objects.get(pk=pk)
+    data['livro'] = livro
+    return render(request,'loja/book_page.html',data)
+
+def compra(request, pk):
+    data = {}
+    livro = Livro.objects.get(pk=pk)
+    data['livro'] = livro
+    return render(request,'loja/compra.html',data)
